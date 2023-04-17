ece4196b84b6c5239a2f92edd40a68ddfdb0990c
theopiedade <theopiedade@hotmail.com>
2023-04-16 23:13:51 -0300

    Server Operating OK

--- scripts.js
+++ scripts.js
@@ -1,56 +1,83 @@
 axios.defaults.headers.common['Authorization'] = '4w9C68vlTtbdtXrsnZR849FV';
-var myIntervalStatus = 0;
-var myIntervalGetMessages = 0;
+var myIntervalStatus;
+var myIntervalGetMessages;
 
 function join_chat () {
+    console.log("Button enter passed Nickname to javascript");
     let nickname = document.querySelector('.chat_nickname_input').value;
     const data = {
         name: nickname
     };
     const query = axios.post('https://mock-api.driven.com.br/api/vm/uol/participants', data);
     query.then(nick_check);
+    query.catch(errorTreat);
 }
 
 function nick_check (answer) {
     console.log(answer.data);
-    if (answer.data === 400) {
+    if (answer.status === 400) {
        alert("Nome já em uso, escolha outro.");
     }
-    if (answer.data === 200) {
-       myIntervalStatus = 0;
-       myIntervalGetMessages = 0;
-       alert("Bem vindo ao chat!");
-       myIntervalStatus = setInterval(keep_status_ok,5000);
-       setInterval(clearInterval(myIntervalStatus),30000);
-       myIntervalGetMessages = setInterval(keep_status_ok,1000);
-       setInterval(clearInterval(myIntervalGetMessages),30000);
-      }
+    if (answer.status === 200) {
+      chat_start();
+    }
+} 
+
+function chat_start() {
+  console("Starting chat...");
+  myIntervalStatus = 0;
+  myIntervalGetMessages = 0;
+  let element = document.querySelector('.chat_status_and_messages');
+  element.innerHTML = '';
+  get_messages();
+  myIntervalStatus = setInterval(keep_status_ok,5000);
+  setInterval(clearInterval(myIntervalStatus),30000);
+  myIntervalGetMessages = setInterval(get_messages,1000);
+  setInterval(clearInterval(myIntervalGetMessages),30000);
+}
+
+
+function get_users () {
+    const query = axios.get('https://mock-api.driven.com.br/api/vm/uol/participants');
+    query.then(show_users);
+    query.catch(errorTreat);
+}
+
+function show_users (users) {
+    console.log(users.data);
 }
 
 function get_messages () {
     const query = axios.get('https://mock-api.driven.com.br/api/vm/uol/messages');
     query.then(server_data_process);
+    query.catch(errorTreat);
 }
 
-function server_data_process (data) {
-    answer.data.forEach(function (item, ind) { message_data_process(item); })
+function server_data_process (answer) {
+    const statusCode = answer.status;
+	console.log(statusCode);
+    console.log(answer.data.length);
+    console.log(answer.data[0]);
+    for (let i=0;i<=answer.data.length;i++) { 
+        message_data_process(answer.data[i]); }
+
 }
 
 
 function message_data_process (item) {
-    if (item.type === "status") {
-        let element = document.querySelector('.chat_open');
+    if (item.type === 'status') {
+        let element = document.querySelector('.chat_status_and_messages');
         element.innerHTML += `
             <div class="container_status">
                 <div class="action">
-                <h1>${item.time}</h1>
-                <p>${item.from}</p> ${text}
+                <h1>(${item.time})</h1>
+                <p>${item.from}</p> ${item.text}
                 </div>
             </div>
         `;
         }
-    if (item.type === "message") {
-        let element = document.querySelector('.chat_open');
+    if (item.type === 'message') {
+        let element = document.querySelector('.chat_status_and_messages');
         element.innerHTML += `
         <div class="container_msg">
             <div class="action">
@@ -60,8 +87,8 @@ function message_data_process (item) {
         </div>
         `;
         }
-    if (item.type === "private_message") {
-        let element = document.querySelector('.chat_open');
+    if (item.type === 'private_message') {
+        let element = document.querySelector('.chat_status_and_messages');
         element.innerHTML += `
         <div class="container_private_msg">
             <div class="action">
@@ -80,3 +107,8 @@ function keep_status_ok (data) {
 function send_messages (message) {
     const query = axios.post('https://mock-api.driven.com.br/api/vm/uol/status', data);
 }
+
+function errorTreat(error) {
+    console.log("Status code: " + error.response.status); // Ex: 404
+    console.log("Error message: " + error.response.data); // Ex: Not Found
+  }
